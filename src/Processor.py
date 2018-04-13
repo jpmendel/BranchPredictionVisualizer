be0977722a4c6d31c1c64dede82222fd295f5cca
Jacob G <jhg023@bucknell.edu>
2018-04-13 17:10:16 -0400

    No need for Processor to have a class at the moment.

--- src/Processor.py
+++ src/Processor.py
@@ -10,68 +10,64 @@ NAME_FROM_REGISTER = [
 ]
 
 
-class Processor:
-    def __init__(self):
-        pass
+def process(instructions):
+    i = 0
 
-    def process(self, instructions):
-        i = 0
+    while i < len(instructions):
+        instruction = instructions[i]
 
-        while i < len(instructions):
-            instruction = instructions[i]
-
-            if instruction.is_nop() or instruction.is_syscall():  # TODO: Handle syscall (for fun?)
-                i += 1
-                continue
-
-            opcode = instruction.get_opcode()
-
-            if opcode == 0x02 or opcode == 0x03:  # TODO: Handle j and jal (change i)
-                raise NotImplementedError('j and jal are not handled!')
-
-            rs = instruction.get_rs()
-            rt = instruction.get_rt()
-
-            if opcode == 0x00:  # R-Type
-                name = NAME_FROM_FUNCT[instruction.get_funct()]
-                rd = instruction.get_rd()
-                shamt = instruction.get_shamt()
-
-                if name == 'add' or name == 'addu':
-                    REGISTERS[rd] = REGISTERS[rs] + REGISTERS[rt]
-                elif name == 'and':
-                    REGISTERS[rd] = REGISTERS[rs] & REGISTERS[rt]
-                elif name == 'jr':
-                    pass  # TODO: Set i accordingly.
-                elif name == 'nor':
-                    REGISTERS[rd] = ~(REGISTERS[rs] | REGISTERS[rt])
-                elif name == 'or':
-                    REGISTERS[rd] = REGISTERS[rs] | REGISTERS[rt]
-                elif name == 'slt' or name == 'sltu':
-                    REGISTERS[rd] = 1 if REGISTERS[rs] < REGISTERS[rt] else 0
-                elif name == 'sll':
-                    REGISTERS[rd] = REGISTERS[rt] << shamt
-                elif name == 'srl':
-                    REGISTERS[rd] = REGISTERS[rt] >> shamt
-                elif name == 'sub' or name == 'subu':
-                    REGISTERS[rd] = REGISTERS[rs] - REGISTERS[rt]
-            else:  # I-Type
-                name = NAME_FROM_OPCODE[opcode]
-                immediate = instruction.get_immediate()
-
-                if name == 'addi' or name == 'addiu':
-                    REGISTERS[rt] = REGISTERS[rs] + immediate
-                elif name == 'andi':
-                    REGISTERS[rt] = REGISTERS[rs] & immediate  # TODO: Zero-extend immediate
-                elif name == 'beq' or name == 'bne':
-                    pass  # TODO: Handle branching
-                elif name == 'ori':
-                    REGISTERS[rt] = REGISTERS[rs] | immediate  # TODO: Zero-extend immediate
-                elif name == 'slti' or name == 'sltiu':
-                    REGISTERS[rt] = 1 if REGISTERS[rs] < immediate else 0
-
-
-            print(instruction)
-            print('Registers:', REGISTERS)
-
+        if instruction.is_nop() or instruction.is_syscall():  # TODO: Handle syscall (for fun?)
             i += 1
+            continue
+
+        opcode = instruction.get_opcode()
+
+        if opcode == 0x02 or opcode == 0x03:  # TODO: Handle j and jal (change i)
+            raise NotImplementedError('j and jal are not handled!')
+
+        rs = instruction.get_rs()
+        rt = instruction.get_rt()
+
+        if opcode == 0x00:  # R-Type
+            name = NAME_FROM_FUNCT[instruction.get_funct()]
+            rd = instruction.get_rd()
+            shamt = instruction.get_shamt()
+
+            if name == 'add' or name == 'addu':
+                REGISTERS[rd] = REGISTERS[rs] + REGISTERS[rt]
+            elif name == 'and':
+                REGISTERS[rd] = REGISTERS[rs] & REGISTERS[rt]
+            elif name == 'jr':
+                pass  # TODO: Set i accordingly.
+            elif name == 'nor':
+                REGISTERS[rd] = ~(REGISTERS[rs] | REGISTERS[rt])
+            elif name == 'or':
+                REGISTERS[rd] = REGISTERS[rs] | REGISTERS[rt]
+            elif name == 'slt' or name == 'sltu':
+                REGISTERS[rd] = 1 if REGISTERS[rs] < REGISTERS[rt] else 0
+            elif name == 'sll':
+                REGISTERS[rd] = REGISTERS[rt] << shamt
+            elif name == 'srl':
+                REGISTERS[rd] = REGISTERS[rt] >> shamt
+            elif name == 'sub' or name == 'subu':
+                REGISTERS[rd] = REGISTERS[rs] - REGISTERS[rt]
+        else:  # I-Type
+            name = NAME_FROM_OPCODE[opcode]
+            immediate = instruction.get_immediate()
+
+            if name == 'addi' or name == 'addiu':
+                REGISTERS[rt] = REGISTERS[rs] + immediate
+            elif name == 'andi':
+                REGISTERS[rt] = REGISTERS[rs] & immediate  # TODO: Zero-extend immediate
+            elif name == 'beq' or name == 'bne':
+                pass  # TODO: Handle branching
+            elif name == 'ori':
+                REGISTERS[rt] = REGISTERS[rs] | immediate  # TODO: Zero-extend immediate
+            elif name == 'slti' or name == 'sltiu':
+                REGISTERS[rt] = 1 if REGISTERS[rs] < immediate else 0
+
+
+        print(instruction)
+        print('Registers:', REGISTERS)
+
+        i += 1
