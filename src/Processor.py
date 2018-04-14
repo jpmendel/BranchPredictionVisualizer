ec5dac064447ebbbb6e94896d1444d0b06e760e9
Jacob G <jhg023@bucknell.edu>
2018-04-13 22:16:52 -0400

    Added beq and bne, but I'm skeptical about the address.

--- src/Processor.py
+++ src/Processor.py
@@ -1,13 +1,18 @@
 from InstructionTypeR import *
 
-REGISTERS = [0] * 32
-
 NAME_FROM_REGISTER = [
     'zero', 'at', 'v0', 'v1', 'a0', 'a1', 'a2', 'a3',
     't0', 't1', 't2', 't3', 't4', 't5', 't6', 't7',
     's0', 's1', 's2', 's3', 's4', 's5', 's6', 's7',
     't8', 't9', 'k0', 'k1', 'gp', 'sp', 's8', 'ra'
 ]
+
+REGISTERS = {key: 0 for key in NAME_FROM_REGISTER}
+
+
+def sign_extend(value, bits):
+    sign_bit = 1 << (bits - 1)
+    return (value & (sign_bit - 1)) - (value & sign_bit)
 
 
 def process(instructions):
@@ -16,7 +21,17 @@ def process(instructions):
     while i < len(instructions):
         instruction = instructions[i]
 
-        if instruction.is_nop() or instruction.is_syscall():  # TODO: Handle syscall (for fun?)
+        if instruction.is_nop():
+            i += 1
+            continue
+
+        if instruction.is_syscall():
+            if REGISTERS['v0'] == 1:  # Print Integer
+                print(REGISTERS['a0'])
+            elif REGISTERS['v0'] == 10:  # Exit Program
+                print('Terminating...')
+                return
+
             i += 1
             continue
 
@@ -25,12 +40,12 @@ def process(instructions):
         if opcode == 0x02 or opcode == 0x03:  # TODO: Handle j and jal (change i)
             raise NotImplementedError('j and jal are not handled!')
 
-        rs = instruction.get_rs()
-        rt = instruction.get_rt()
+        rs = NAME_FROM_REGISTER[instruction.get_rs()]
+        rt = NAME_FROM_REGISTER[instruction.get_rt()]
 
         if opcode == 0x00:  # R-Type
             name = NAME_FROM_FUNCT[instruction.get_funct()]
-            rd = instruction.get_rd()
+            rd = NAME_FROM_REGISTER[instruction.get_rd()]
             shamt = instruction.get_shamt()
 
             if name == 'add' or name == 'addu':
@@ -38,7 +53,8 @@ def process(instructions):
             elif name == 'and':
                 REGISTERS[rd] = REGISTERS[rs] & REGISTERS[rt]
             elif name == 'jr':
-                pass  # TODO: Set i accordingly.
+                i = REGISTERS[rs]
+                continue
             elif name == 'nor':
                 REGISTERS[rd] = ~(REGISTERS[rs] | REGISTERS[rt])
             elif name == 'or':
@@ -59,13 +75,20 @@ def process(instructions):
                 REGISTERS[rt] = REGISTERS[rs] + immediate
             elif name == 'andi':
                 REGISTERS[rt] = REGISTERS[rs] & immediate  # TODO: Zero-extend immediate
-            elif name == 'beq' or name == 'bne':
-                pass  # TODO: Handle branching
+            elif name == 'beq':
+                if REGISTERS[rs] == REGISTERS[rt]:
+                    i += sign_extend(immediate, 15)
+                    continue
+            elif name == 'bne':
+                if REGISTERS[rs] != REGISTERS[rt]:
+                    i += sign_extend(immediate, 15)
+                    continue
+            elif name == 'lui':
+                REGISTERS[rt] = immediate << 16
             elif name == 'ori':
                 REGISTERS[rt] = REGISTERS[rs] | immediate  # TODO: Zero-extend immediate
             elif name == 'slti' or name == 'sltiu':
                 REGISTERS[rt] = 1 if REGISTERS[rs] < immediate else 0
-
 
         print(instruction)
         print('Registers:', REGISTERS)
